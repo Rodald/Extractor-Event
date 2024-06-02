f35a6173312cb18dd5f7a31d0e6ee94daf10d045
Rodald <poltdavid@icloud.com>
2024-06-02 20:03:47 +0200

    updated GameSpectator.java

--- src/main/java/net/rodald/event/GameSpectator.java
+++ src/main/java/net/rodald/event/GameSpectator.java
@@ -1,8 +1,6 @@
 package net.rodald.event;
 
-import org.bukkit.GameMode;
 import org.bukkit.entity.Player;
-import org.bukkit.scheduler.BukkitRunnable;
 
 public class GameSpectator {
     public static void setSpectator(Player player, Boolean mode) {
