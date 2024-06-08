3a356365f02dbda3b1822f98020847c1f6569f12
Rodald <poltdavid@icloud.com>
2024-06-08 11:43:23 +0200

    players are now in adventure

--- src/main/java/net/rodald/event/GameSpectator.java
+++ src/main/java/net/rodald/event/GameSpectator.java
@@ -1,5 +1,6 @@
 package net.rodald.event;
 
+import org.bukkit.GameMode;
 import org.bukkit.entity.Player;
 import org.bukkit.metadata.FixedMetadataValue;
 import org.bukkit.plugin.Plugin;
@@ -12,15 +13,17 @@ public class GameSpectator {
         this.plugin = plugin;
     }
         public static void setSpectator(Player player, Boolean mode) {
+            if (!player.hasMetadata("game_spectator")) {
+                player.setMetadata("game_spectator", new FixedMetadataValue(plugin, true));
+                player.setGameMode(GameMode.ADVENTURE);
+            } else {
+                player.removeMetadata("game_spectator", plugin);
+                player.setGameMode(GameMode.SURVIVAL);
+            }
             player.setAllowFlight(mode);
             player.setInvisible(mode);
             player.setInvulnerable(mode);
-
-            if (!player.hasMetadata("game_spectator")) {
-                player.setMetadata("game_spectator", new FixedMetadataValue(plugin, true));
-            } else {
-                player.removeMetadata("game_spectator", plugin);
-            }
+;
         }
 
         public static boolean getSpectator(Player player) {
