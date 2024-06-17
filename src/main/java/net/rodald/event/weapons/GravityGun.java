360421fa8e2cc08781a71ad85d1e745c1f08633e
Rodald <poltdavid@icloud.com>
2024-06-17 22:20:40 +0200

    gravity gun basics

--- src/main/java/net/rodald/event/weapons/GravityGun.java
+++ src/main/java/net/rodald/event/weapons/GravityGun.java
@@ -51,54 +51,20 @@ public class GravityGun implements Listener {
                 event.getAction().toString().contains("RIGHT")) {
 
             UUID playerId = player.getUniqueId();
-            long currentTime = System.currentTimeMillis();
-            long lastUseTime = cooldowns.getOrDefault(playerId, 0L);
 
-            if (currentTime >= lastUseTime + COOLDOWN_DURATION) {
-                // Aktiviere oder deaktiviere das Force Field
-                toggleForceField(player);
-                cooldowns.put(playerId, currentTime);
-            } else {
-                player.sendMessage(ChatColor.RED + "Force Field is on cooldown. Please wait.");
-            }
-
+            gravityGunTick(Player player);
             event.setCancelled(true); // Verhindert, dass das Item in die Hand genommen wird
         }
     }
 
-    private void toggleForceField(Player player) {
-        UUID playerId = player.getUniqueId();
-        boolean isActive = forceFieldActive.getOrDefault(playerId, false);
-
-        if (!isActive) {
-            activateForceField(player);
-        } else {
-            deactivateForceField(player);
-        }
-    }
-
-    private void activateForceField(Player player) {
+    private void gravityGunTick(Player player) {
         player.sendMessage(ChatColor.RED + "Activated Force Field");
-        forceFieldActive.put(player.getUniqueId(), true);
-
-        for (ItemStack item : player.getInventory().getContents()) {
-            if (item != null && item.getItemMeta().getDisplayName().equals(GRAVITY_GUN_ITEM_NAME)) {
-                ItemMeta forceFieldMeta = item.getItemMeta();
-                forceFieldMeta.addEnchant(Enchantment.INFINITY, 1, true);
-                item.setItemMeta(forceFieldMeta);
-            }
-        }
-
-        new BukkitRunnable() {
             Location playerLoc = player.getLocation().clone();
             int radius = 7;
             double additionalY = -0.1; // Höhe hinzufügen
             double maxYVelocity = 0.6; // Maximale Y-Geschwindigkeit festlegen
 
-            @Override
-            public void run() {
                 if (!forceFieldActive.getOrDefault(player.getUniqueId(), false) || !player.isOnline()) {
-                    cancel();
                     forceFieldActive.put(player.getUniqueId(), false);
                     return;
                 }
@@ -109,13 +75,8 @@ public class GravityGun implements Listener {
                 Vector currentVelocity = player.getVelocity();
                 Vector direction;
                 // slows player down when he is sneaking
-                if (player.isSneaking()) {
-                    direction = player.getLocation().getDirection().normalize().setY(0); // Richtung, in die der Spieler schaut
-                } else if (player.isSprinting()) {
-                    direction = player.getLocation().getDirection().normalize().multiply(2); // Richtung, in die der Spieler schaut
-                } else {
-                    direction = new Vector(0, 0, 0);
-                }
+                direction = player.getLocation().getDirection().normalize(); // Richtung, in die der Spieler schaut
+
 
                 // Füge dem Y-Vektor des Spielers Velocity hinzu
                 currentVelocity.setY(currentVelocity.getY() + additionalY);
@@ -130,34 +91,11 @@ public class GravityGun implements Listener {
                 double forceMultiplier = 0.0005; // Stärke der Abstoßung anpassen
                 double forceHeightMultiplier = 1; // Stärke der Abstoßung anpassen
 
-                // Abstoßung von festen Blöcken im Radius
-                for (double x = -radius; x <= radius; x++) {
-                    for (double y = -radius; y <= radius; y++) {
-                        for (double z = -radius; z <= radius; z++) {
-                            Location blockLoc = playerLoc.clone().add(x, y, z);
-                            Block block = blockLoc.getBlock();
-
-                            // Überprüfe, ob der Spieler in der Nähe von festen Blöcken ist
-                            if (!block.isEmpty() && (block.getType().isSolid())) {
-                                // Berechne die Richtung vom Block zum Spieler
-                                Vector blockDirection = playerLoc.toVector().subtract(blockLoc.toVector()).normalize();
-
-                                // Bewege den Spieler von den Blöcken weg
-                                currentVelocity.add(blockDirection.multiply(forceMultiplier));
-                                currentVelocity.setY(currentVelocity.getY() * forceHeightMultiplier);
-                            }
-
-                            if (!block.isEmpty() && (block.isLiquid() && y <= radius)) {
-                                // Berechne die Richtung vom Block zum Spieler
-                                Vector blockDirection = playerLoc.toVector().subtract(blockLoc.toVector()).normalize();
 
                                 // Bewege den Spieler von den Blöcken weg
                                 currentVelocity.setY(currentVelocity.getY() + forceMultiplier);
                                 currentVelocity.setY(currentVelocity.getY() * forceHeightMultiplier);
-                            }
-                        }
-                    }
-                }
+
 
                 if (playerLoc.getBlock().getType() == Material.WATER) {
                     currentVelocity.setY(currentVelocity.getY() + 1);
@@ -168,8 +106,7 @@ public class GravityGun implements Listener {
 
                 // Aktualisiere die Spielerposition für die nächste Iteration
                 playerLoc = newLoc;
-            }
-        }.runTaskTimer(plugin, 0, 1); // Task alle 1 Tick ausführen
+
     }
 
     private void deactivateForceField(Player player) {
